dc176e9a4955a8e2899da13709c40080a1c346fa
Nandini-9908 <megavathnandini3@gmail.com>
2025-04-29 21:47:07 +0530

    Create customer.cpp

---
+++ customer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+// Node for each call log
+struct CallLogNode {
+    string customerName;
+    time_t timestamp;
+    CallLogNode* next;
+    CallLogNode* prev; // Optional, if you want bidirectional navigation
+
+    CallLogNode(string name) {
+        customerName = name;
+        timestamp = time(nullptr); // current time
+        next = nullptr;
+        prev = nullptr;
+    }
+};
