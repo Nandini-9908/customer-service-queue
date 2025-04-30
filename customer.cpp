5a155737208690d08a1aecf68fddbe54580aebcc
Chandana-dharshanam <dharshanamchandana@gmail.com>
2025-04-30 18:02:10 +0530

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -15,9 +15,9 @@ struct CallLogNode
     CallLogNode(string name)
   {
         customerName = name;
-        timestamp = time(nullptr); // current time
-        next = nullptr;
-        prev = nullptr;
+        timestamp = time(NULL); // current time
+        next = NULL;
+        prev = NULL;
     }
 };
 //call log queue class
@@ -30,14 +30,14 @@ private:
 public:
     CallLogQueue()
     {
-        head = nullptr;
-        tail = nullptr;
+        head = NULL;
+        tail = NULL;
     }
 
 // Add a new call to the end of the Queue
 void addCall(string customerName)
 {
-    CallLogNode* newCall=new callLogNOde(customerName);
+    CallLogNode* newCall=new CallLogNode(customerName);
 
     if(!head) // Empty List
     {
@@ -95,6 +95,46 @@ void displayLogs()
      }
   }
 };
+// Main function to test the CallLogQueue
+int main() {
+    CallLogQueue callQueue;
+    int choice;
+    string name;
+
+    do {
+        cout << "\n--- Call Log System ---\n";
+        cout << "1. Add Call\n";
+        cout << "2. Remove Oldest Call\n";
+        cout << "3. Display Call Logs\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        cin.ignore(); // to consume the newline after number input
+
+        switch (choice) {
+            case 1:
+                cout << "Enter customer name: ";
+                getline(cin, name);
+                callQueue.addCall(name);
+                break;
+            case 2:
+                callQueue.removeOldestCall();
+                break;
+            case 3:
+                callQueue.displayLogs();
+                break;
+            case 4:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice. Try again.\n";
+        }
+    } while (choice != 4);
+
+    return 0;
+}
+
 
 
 
