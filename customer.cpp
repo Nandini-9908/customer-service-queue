1ec51fb503696f86fd79bb74c2031cfefbce1c3a
Nandini-9908 <megavathnandini3@gmail.com>
2025-04-30 07:01:46 +0530

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -5,13 +5,15 @@
 using namespace std;
 
 // Node for each call log
-struct CallLogNode {
+struct CallLogNode
+{
     string customerName;
     time_t timestamp;
     CallLogNode* next;
     CallLogNode* prev; // Optional, if you want bidirectional navigation
 
-    CallLogNode(string name) {
+    CallLogNode(string name)
+  {
         customerName = name;
         timestamp = time(nullptr); // current time
         next = nullptr;
@@ -19,15 +21,35 @@ struct CallLogNode {
     }
 };
 //call log queue class
-class CallLogQueue {
+class CallLogQueue
+{
 private:
     CallLogNode* head;
     CallLogNode* tail;
 
 public:
-    CallLogQueue() {
+    CallLogQueue()
+    {
         head = nullptr;
         tail = nullptr;
     }
+
+// Add a new call to the end of the Queue
+void addCall(string customerName)
+{
+    callLogNode* newCall=new callLogNOde(customerName);
+
+    if(!head)
+    {
+        head = tail=newCall;
+    }
+    else
+    {
+        tail->next=newCall;
+        newCall->prev=tail;
+        tail=newCall;
+    }
+   cout<< "Call From "<<customerName <<" Added At "<<ctime(&newCall->timestamp);
+}
 
 
